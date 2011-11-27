fcbb57513a4bc830eab56100e275a92371c27a12
Rohit Eipe <pineapplepizza80@gmail.com>
2011-11-26 17:00:07 -0800

    Initial commit 	new file:   .gitignore 	new file:   Gemfile 	new file:   Gemfile.lock 	new file:   README 	new file:   Rakefile 	new file:   app/assets/images/rails.png 	new file:   app/assets/javascripts/application.js 	new file:   app/assets/javascripts/welcome.js.coffee 	new file:   app/assets/stylesheets/application.css 	new file:   app/assets/stylesheets/welcome.css.scss 	new file:   app/controllers/application_controller.rb 	new file:   app/controllers/welcome_controller.rb 	new file:   app/helpers/application_helper.rb 	new file:   app/helpers/welcome_helper.rb 	new file:   app/mailers/.gitkeep 	new file:   app/models/.gitkeep 	new file:   app/views/layouts/application.html.erb 	new file:   app/views/welcome/index.html.erb 	new file:   config.ru 	new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/initializers/wrap_parameters.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   db/seeds.rb 	new file:   doc/README_FOR_APP 	new file:   lib/assets/.gitkeep 	new file:   lib/tasks/.gitkeep 	new file:   log/.gitkeep 	new file:   public/404.html 	new file:   public/422.html 	new file:   public/500.html 	new file:   public/favicon.ico 	new file:   public/index.html.foo 	new file:   public/robots.txt 	new file:   script/rails 	new file:   test/fixtures/.gitkeep 	new file:   test/functional/.gitkeep 	new file:   test/functional/welcome_controller_test.rb 	new file:   test/integration/.gitkeep 	new file:   test/performance/browsing_test.rb 	new file:   test/test_helper.rb 	new file:   test/unit/.gitkeep 	new file:   test/unit/helpers/welcome_helper_test.rb 	new file:   vendor/assets/stylesheets/.gitkeep 	new file:   vendor/plugins/.gitkeep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridemo::Application.config.secret_token = '03e3fb186646ce8187270f113868bd563fb1bc345ab597e0fae0892b14b5aee115010cc66aff23a6522db56872c032014f6b890634134bf65b2fc66806bfdf1a'
